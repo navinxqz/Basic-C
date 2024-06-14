956c1b7c4895a8bfd50c41c7c076d5b3cec5bfed
navin-nawshin <navinmdnawshin@gmail.com>
2024-06-14 21:10:06 +0600

    add sub mul div added

--- NumSum.c
+++ NumSum.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 int main(){
     int a,b,sum;
-    printf("Input 2 number: ");
+    printf("Lets do addition\n");
+    printf("Input 2 num: ");
     scanf("%d %d",&a, &b);
 
     sum = a+b;
+    printf("Total: %d\n",sum);
+
+    printf("Lets do substraction\n");
+    printf("Input 2 num: ");
+    scanf("%d %d",&a, &b);
+
+    sum = a-b;
+    printf("Total: %d\n",sum);
+
+    printf("Lets do multiplication\n");
+    printf("Input 2 num: ");
+    scanf("%d %d",&a, &b);
+
+    sum = a*b;
+    printf("Total: %d\n",sum);
+
+    printf("Lets do division\n");
+    printf("Input 2 num: ");
+    scanf("%d %d",&a, &b);
+
+    sum = a/b;
     printf("Total: %d\n",sum);
     return 0;
 }
